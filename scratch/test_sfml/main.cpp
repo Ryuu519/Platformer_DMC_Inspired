af7c03fff407c9b868230310c3da624ac99f8ec6
Ryuu519 <adrian45.rusu@gmail.com>
2026-05-22 22:03:41 +0300

    Remove scratch/test_sfml from repo and add scratch/ to .gitignore

--- scratch/test_sfml/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <SFML/Graphics.hpp>
-#include <iostream>
-
-int main() {
-    std::cout << "SFML test starting..." << std::endl;
-    sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
-    std::cout << "Desktop resolution: " << desktop.width << "x" << desktop.height << std::endl;
-    return 0;
-}
